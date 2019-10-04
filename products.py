225493727595dc0bc647466a8159c19c7d692f7a
Owen Chang <owenchang@m-toy.com.tw>
2019-10-04 22:56:55 +0800

    add utf-8

--- products.py
+++ products.py
@@ -14,6 +14,15 @@ for p in products:
 	#print(p[0])
 	print(p[0] , '的價格為' , p[1])
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品, 價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
+
+
+data = [1, 3, 5, 7, 9] # 清單中裝著一些整數
+# 請開始寫"寫入檔案"的程式碼
+with open('test.txt', 'w') as f1:
+	for d in data:
+		d = str(d)
+		f1.write(d + '\n')
