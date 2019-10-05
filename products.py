b26fe7d6f11b55e7b8492de42431531b532a6051
Owen Chang <owenchang@m-toy.com.tw>
2019-10-05 22:47:12 +0800

    add read & explort csv

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r' , encoding='utf-8') as f:
+	for line in f:
+		if '品名,價格' in line:
+			continue#繼續
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+#使用者輸入區段
 while  True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
@@ -9,20 +19,23 @@ while  True:
 	#p.append(price)
 	products.append([name , price])
 print(products)
+
+#印出所有購買紀錄
 for p in products:
 	#print(p)
 	#print(p[0])
 	print(p[0] , '的價格為' , p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品, 價格\n')
+	f.write('品名, 價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
 
 
-data = [1, 3, 5, 7, 9] # 清單中裝著一些整數
+#data = [1, 3, 5, 7, 9] # 清單中裝著一些整數
 # 請開始寫"寫入檔案"的程式碼
-with open('test.txt', 'w') as f1:
-	for d in data:
-		d = str(d)
-		f1.write(d + '\n')
+#with open('test.txt', 'w') as f1:
+	#for d in data:
+		#d = str(d)
+		#f1.write(d + '\n')
