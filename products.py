1c7b3a7555fd7ce351eab7cd8600fdb0e10482fd
Owen Chang <owenchang@m-toy.com.tw>
2019-10-06 12:20:04 +0800

    refactor

--- products.py
+++ products.py
@@ -1,10 +1,10 @@
 import os # operating system
 
-products = []
-if os.path.isfile('products.csv'):#檢查檔案
-	print('yeah! 找到檔案了')
-	#讀取檔案
-	with open('products.csv', 'r' , encoding='utf-8') as f:
+
+#讀取檔案
+def read_file(filename):
+	products = []
+	with open(filename, 'r' , encoding='utf-8') as f:
 		for line in f:
 			if '品名, 價格' in line:
 				#print('有進去呦～')
@@ -12,34 +12,50 @@ if os.path.isfile('products.csv'):#檢查檔案
 			name, price = line.strip().split(',')
 			products.append([name, price])
 	print(products)
-
-else:
-	print('找不到舊資料，將建立新檔案')
+	return products
 
 #使用者輸入區段
-while  True:
-	name = input('請輸入商品名稱：')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格：')
-	#p =[]
-	#p.append(name)
-	#p.append(price)
-	products.append([name , price])
-print(products)
+def user_input(products):
+	while  True:
+		name = input('請輸入商品名稱：')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格：')
+		#p =[]
+		#p.append(name)
+		#p.append(price)
+		products.append([name , price])
+	print(products)
+	return products
 
 #印出所有購買紀錄
-for p in products:
-	#print(p)
-	#print(p[0])
-	print(p[0] , '的價格為' , p[1])
+def print_products(products):
+	for p in products:
+		#print(p)
+		#print(p[0])
+		print(p[0] , '的價格為' , p[1])
 
 #寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('品名, 價格\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+	with open('filename', 'w', encoding='utf-8') as f:
+		f.write('品名, 價格\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
 
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):#檢查檔案
+		print('yeah! 找到檔案了')
+		products = read_file(filename)
+	else:
+		print('找不到舊資料，將建立新檔案')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
 
 
 
